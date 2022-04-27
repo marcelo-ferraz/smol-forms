5ceb394a40e4457fe6a824ed2a13f5c1df2665f1
Marcelo Ferraz <marcelosfm@gmail.com>
2022-04-27 21:49:52 +1200

    project files

---
+++ .eslintrc.js
@@ -0,0 +1,114 @@
+const { off } = require("process");
+
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true
+    },
+    globals: {
+    },
+    extends: [        
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'airbnb',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+        tsconfigRootDir: './'
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    plugins: [
+        'react',
+        'react-hooks',
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', 4, {'SwitchCase': 1}],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-filename-extension': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'class-methods-use-this': 'off',
+        'react/require-default-props': 'off',
+        'react/forbid-prop-types': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'no-use-before-define': 'off',
+        'no-tabs': 'off',
+        'linebreak-style': 'off',
+        'max-len': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+    },
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx']
+        },
+        'import/resolver': {
+            typescript: {
+                project: '.',
+            }
+        },
+    },
+    overrides: [{
+        // applying the extra sets of rules that ts files require
+        files: ['./src/**/*+(.ts|.tsx)'],
+        excludedFiles: ['*.js', '*.jsx'],
+        extends: [
+            'plugin:@typescript-eslint/recommended',
+        ],
+        rules: {
+            'react/prop-types': 'off',
+        },
+    }, {
+        // Those are for the files used to build and transpile the app.
+        // without this, it complains that some dependencies 
+        // should not be on dev-dependencies, which is wrong
+        // any .js file inside the 'scripts' or 'config' folders is used for building.
+        // it only makes sense to have consoles there
+        files: [
+            './*+(.js|.ts)',
+            './scripts/**/*+(.js|.ts)', 
+            './config/**/*+(.js|.ts)'
+        ],
+        rules: {
+            'no-console': 'off',
+            'import/no-extraneous-dependencies': 'off',
+        },
+    }, {
+        // tests usually need a more loose set of rules
+        files: [
+            './src/**/*.+(test|spec)+(.js|.ts|.jsx|.tsx)',            
+        ],
+        env: {
+            jest: true,
+        },
+        rules: {
+            'import/no-extraneous-dependencies': 'off',
+            'class-methods-use-this': 'off',
+            'react/require-default-props': 'off',
+            'react/forbid-prop-types': 'off',
+            'react/jsx-props-no-spreading': 'off',
+            'no-use-before-define': 'off',
+            'no-tabs': 'off',
+            'react/prop-types': 'off',
+        },
+    }],
+};
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const getTsandJsAliases = require('./config/getTsandJsAliases');
+const paths = require('./config/paths');
+const externalDependencies = require('./config/externalDependencies');
+
+const {
+    imagesRule,
+    fontsRule,
+    tsJsRule,
+    jsSrcMapRule,
+    getCssRules,
+} = require('./config/rules');
+
+module.exports = (env, argv = {}) => {
+    const isProduction = argv.mode && argv.mode !== 'development';
+
+    const [
+        cssModulesRule,
+        cssRule,
+        thirdPartyCssRule,
+    ] = getCssRules([paths.src], isProduction);
+
+    return {
+        entry: {
+            index: './src/index',
+            validators: './src/validators',
+            types: './src/types',
+        },
+
+        mode: argv.mode || 'development',
+
+        context: paths.context,
+
+        output: {
+            path: paths.output,
+            library: {
+                name: 'smol-forms',
+                type: 'umd',
+            },
+            clean: true,
+        },
+
+        module: {
+            rules: [
+                // compiles any of the js, jsx, ts, tsx
+                // for ts: creates the types and map files
+                tsJsRule,
+                // creates the source maps for js files
+                jsSrcMapRule,
+                // any files named *.module.css or *.module.scss
+                // will be compiled into css modules
+                cssModulesRule,
+                // transform the scss and css files and bundle them into css files
+                cssRule,
+                // extracts and bundle from node_modules css only files
+                thirdPartyCssRule,
+                // places the images on the proper folder and fix the urls
+                imagesRule,
+                // places the fonts on the proper folder and fix the urls
+                fontsRule,
+            ],
+        },
+
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+            },
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                linkType: 'text/css',
+                filename: 'css/[name].css',
+                chunkFilename: 'css/[name].chunk.css',
+            }),
+            new BundleAnalyzerPlugin({
+                analyzerMode: env.analyze ? 'server' : 'disabled',
+            }),
+        ],
+
+        resolve: {
+            extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
+            alias: getTsandJsAliases(),
+        },
+
+        externals: externalDependencies,
+    };
+};
