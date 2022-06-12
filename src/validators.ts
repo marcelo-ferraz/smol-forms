baec9d5a0d27c5b34b51ab86bbcfa4362c326473
marcelo-ferraz <marcelosfm@gmail.com>
2022-06-12 22:49:34 +1200

    linting away

--- src/validators.ts
+++ src/validators.ts
@@ -3,7 +3,7 @@ export const regexes = {
     // eslint-disable-next-line no-control-regex
     isEmail: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
     isInt: /^\d*$/,
-    isFloat: (decimal: number) => new RegExp(`^\d*\.?\d{0,${decimal}}$`),
+    isFloat: (decimal: number) => new RegExp(`^\\d*\\.?\\d{0,${decimal}}$`),
 };
 
 export const isRequired = (
@@ -17,19 +17,19 @@ export const isInt = (
 ) => (regexes.isInt.test(val?.toString().trim() ?? '') ? msg : null);
 
 export const isFloat = (decimal = 2) => (
-    val: unknown,
+    { toString }: unknown = '',
     msg = 'This field is number',
 ) => (regexes.isFloat(decimal).test(
-    val?.toString().trim() ?? '',
+    toString().trim() ?? '',
 ) ? msg : null);
 
 export const pattern = (
     regex: RegExp,
 ) => (
-    val: unknown,
+    { toString }: unknown = '',
     msg = 'This field is incorrect',
 ) => (new RegExp(regex).test(
-    val?.toString().trim() ?? '',
+    toString().trim() ?? '',
 ) ? msg : null);
 
 export const isEmail = (
